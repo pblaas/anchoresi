c5e1fb24fe495a80ed65ddc3235b839419d4a58f
pblaas <patrick@kite4fun.nl>
2018-04-23 15:28:53 +0200

    update viewheight usage

--- app.py
+++ app.py
@@ -105,4 +105,4 @@ def images():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, threaded=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
