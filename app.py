45d93a62e0aff047eea97d03d7fb4ca2a5954e7d
pblaas <patrick@kite4fun.nl>
2018-04-23 19:35:22 +0200

    disable debug mode

--- app.py
+++ app.py
@@ -115,4 +115,4 @@ def images():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, threaded=True, host='0.0.0.0', port=5000)
